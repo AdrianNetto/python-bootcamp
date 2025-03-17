99e63d276452e12609ab669ef70ba05d2e1d3b8e
AdrianNetto <adrian.netto73@gmail.com>
2025-03-17 10:03:19 -0300

    feat: Numbers and operators

---
+++ Python Numbers/main.py
@@ -0,0 +1,26 @@
+# Sum
+print(3 + 4)
+
+# Subtraction
+print(4 - 3)
+
+# Division
+print(4 / 2)
+
+# Multiplication
+print(4 * 2)
+
+# Modulo or "Mod" Operator
+print(7 % 3) # returns 3
+
+# - How to know if a number is even or odd
+print(23 % 2) # make "% 2", and need to return 0 to be even.
+
+# Powers
+print(2 ** 3)
+
+# Resolve first some part of the calc
+print(2 + 10 * 10 + 3) # Follow the PEMDAS pattern, resolving first the multiplication, and it returns 105.
+
+print((2 + 10) * (10 + 3)) # Also follow the PEMDAS pattern, but now it resolves first the parentheses, and  this retuns 156.
+
