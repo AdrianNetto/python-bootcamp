3e3f9f987482856c0cfac4ddcb244f912cf42494
AdrianNetto <adrian.netto73@gmail.com>
2025-03-18 09:18:41 -0300

    feat: string properties and methods

---
+++ String Properties and Methods/main.py
@@ -0,0 +1,26 @@
+name = 'Sam'
+
+# name[0] = 'P' --> Not valid
+
+last_letters = name[1:]
+
+print(last_letters)
+
+new_name = f'P{last_letters}'
+
+print(new_name)
+
+
+letter = 'z'
+
+print(letter * 10)
+
+x = 'Hello World'
+
+print(x.upper())
+print(x.lower())
+
+x = 'Hi this is a string'
+
+print(x.split())
+print(x.split('i'))
