9255b9596310feb1f1aa64d366d2d5f10b1fa836
AdrianNetto <adrian.netto73@gmail.com>
2025-03-20 09:20:27 -0300

    feat: comparison operators

---
+++ Module 2/Comparison Operators/main.py
@@ -0,0 +1,13 @@
+print(2 == 2) # == is equality, = is to assign variables
+print(1 == 2)
+print('hello' == 'bye')
+print('Bye' == 'bye')
+print(2.0 == 2)
+
+print('\n\n', 3 != 3)
+print(4 != 5)
+
+print('\n\n', 2 > 1)
+print(2 < 1)
+print(2 <= 2)
+print(2 >= 1)
